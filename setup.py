1247dd588f4139007a2806344db7dda64f8a0170
Shinya <takatelin01@icloud.com>
2020-02-05 18:53:33 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os, sys
+from setuptools import setup, find_packages
+
+def read_requirements():
+    """Parse requirements from requirements.txt."""
+    reqs_path = os.path.join('.', 'requirements.txt')
+    with open(reqs_path, 'r') as f:
+        requirements = [line.rstrip() for line in f]
+    return requirements
+
+setup(
+    name='get_response',
+    version='0.0.1',
+    description='get response for Crawling',
+    author='Shinya',
+    author_email='shinya33001@gmail.com',
+    install_requires=read_requirements(),
+    url='https://github.com/newyee/get_response',
+    license='MIT',
+)
